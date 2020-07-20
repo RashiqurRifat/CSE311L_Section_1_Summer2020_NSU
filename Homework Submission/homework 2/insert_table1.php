94660224f471a369639a3c398c28c9ad8fcef361
RashiqurRifat <rashiqur.rifat@northsouth.edu>
2020-07-20 17:11:10 +0600

    insert_table commit

---
+++ Homework Submission/homework 2/insert_table1.php
@@ -0,0 +1,31 @@
+<?php
+
+$link = mysqli_connect('localhost','root','','VS_CODE_TEST');
+
+if($link == false)
+{
+    die("Error: Could not Connect." .mysqli_connect_error());
+}
+
+$sql = "INSERT INTO departments
+VALUES
+(10,'Administration',200,1700),
+(20,'Marketing',201,1800),
+(50,'Shipping',124,1500),
+(60,'IT',103,1400),
+(80,'Sales',149,2500),
+(90,'Executive',100,1700),
+(110,'Accounting',205,1700),
+(190,'Contracting',NULL,1700)";
+
+if (mysqli_query($link,$sql))
+{
+ echo "VALUES INSERTED";
+}
+ else
+ {
+   echo "Error : Could not able to create Table." .mysqli_error($link);
+ }
+ mysqli_close($link)
+
+?>
